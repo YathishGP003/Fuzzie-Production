e241ed9270b19c5d030b8a5012a76973424b6eb2
YathishGP003 <yathishgp1@gmail.com>
2024-10-01 21:35:47 +0530

    uploadcare error

--- src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -43,9 +43,7 @@
 'use client'
 import React, { useEffect, useRef } from 'react'
 import * as LR from '@uploadcare/blocks'
-import { useRouter } from 'next/navigation'
-import { FileUploaderRegular } from '@uploadcare/react-uploader';
-import '@uploadcare/react-uploader/core.css';
+import { useRouter } from 'next/navigation' 
 
 type Props = {
   onUpload: (e: string) => any
@@ -67,16 +65,26 @@ const UploadCareButton = ({ onUpload }: Props) => {
       }
     }
     // ctxProviderRef.current.addEventListener('file-upload-success', handleUpload)
+     ctxProviderRef.current?.addEventListener('file-upload-success', handleUpload)
   }, [])
 
   return (
     <div>
-      <FileUploaderRegular
-         sourceList="local, url, camera, dropbox"
-         classNameUploader="uc-light"
-         pubkey="2614df91112b67078ce2"
+      <lr-config
+        ctx-name="my-uploader"
+        pubkey="2614df91112b67078ce2"
       />
-    </div>
+
+      <lr-file-uploader-regular
+        ctx-name="my-uploader"
+        css-src={`https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.35.2/web/lr-file-uploader-regular.min.css`}
+      />
+
+      <lr-upload-ctx-provider
+        ctx-name="my-uploader"
+        ref={ctxProviderRef}
+      />
+  </div>
   )
 }
 
